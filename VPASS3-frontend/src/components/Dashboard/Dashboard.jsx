dea1b9227a817cf8e0aafdd9ac1b4d0f89ff65ba
gerardoluceroc <gerardo.lucero.c@usach.cl>
2025-04-16 17:04:26 -0400

    Trabajando en vista de home y el dashboard

---
+++ VPASS3-frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,76 @@
+import { Box, ButtonBase, Typography } from "@mui/material";
+import "./Dashboard.css";
+import PersonPlus from "./../../assets/person-plus.png"
+import ContactanosImage from "./../../assets/contactanos.png"
+import DescargarRegistrosImage from "./../../assets/descargaRegistros.png"
+import ConfiguracionImage from "./../../assets/configuracion.png"
+import BlacklistImage from "./../../assets/blacklist.png"
+import UltimosRegistrosImage from "./../../assets/ultimosregistros.png"
+const Dashboard = () => {
+  return (
+    <Box id="ContainerDashboard">
+        <ButtonBase className="ItemDashboard" onClick={()=>{}} >
+            <Box
+            component="img"
+            src={PersonPlus}
+            alt="Nueva visita"
+            className="ImagenNuevaVisitaDashboard"
+            />
+            <Typography className="TituloItemDashboard">Nueva Visita</Typography>
+        </ButtonBase>
+
+        <ButtonBase className="ItemDashboard" onClick={()=>{}}>
+            <Box
+            component="img"
+            src={UltimosRegistrosImage}
+            alt="Últimos Registros"
+            className="ImagenNuevaVisitaDashboard"
+            />
+            <Typography className="TituloItemDashboard">Últimos Registros</Typography>
+        </ButtonBase>
+
+        <ButtonBase className="ItemDashboard" onClick={()=>{}}>
+            <Box
+            component="img"
+            src={BlacklistImage}
+            alt="Lista Negra"
+            className="ImagenNuevaVisitaDashboard"
+            />
+            <Typography className="TituloItemDashboard">Lista Negra</Typography>
+        </ButtonBase>
+
+        <ButtonBase className="ItemDashboard" onClick={()=>{}}>
+            <Box
+            component="img"
+            src={DescargarRegistrosImage}
+            alt="Descargar Registros"
+            className="ImagenNuevaVisitaDashboard"
+            />
+            <Typography className="TituloItemDashboard">Descargar Registros</Typography>
+        </ButtonBase>
+
+        <ButtonBase className="ItemDashboard" onClick={()=>{}}>
+            <Box
+            component="img"
+            src={ContactanosImage}
+            alt="Contáctanos"
+            className="ImagenNuevaVisitaDashboard"
+            />
+            <Typography className="TituloItemDashboard">Contáctanos</Typography>
+        </ButtonBase>
+
+        <ButtonBase className="ItemDashboard" onClick={()=>{}}>
+            <Box
+            component="img"
+            src={ConfiguracionImage}
+            alt="Ajustes"
+            className="ImagenNuevaVisitaDashboard"
+            />
+            <Typography className="TituloItemDashboard">Ajustes</Typography>
+        </ButtonBase>
+
+    </Box>
+  )
+}
+
+export default Dashboard;
