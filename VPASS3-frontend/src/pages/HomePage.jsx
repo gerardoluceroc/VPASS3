a619ad149a30a26bff9d6296904306bbacb77879
gerardoluceroc <gerardo.lucero.c@usach.cl>
2025-05-07 17:20:46 -0400

    eliminacion de codigo en desuso

--- VPASS3-frontend/src/pages/HomePage.jsx
+++ VPASS3-frontend/src/pages/HomePage.jsx
@@ -8,18 +8,6 @@ const HomePage = () => {
   InterceptorResponse();
   InterceptorRequest();
 
-  useEffect(() => {
-    axios.get('http://localhost:5113/User/all')
-      .then(response => {
-        // Manejar la respuesta
-        console.log(response.data);
-      })
-      .catch(error => {
-        // Manejar errores
-        console.error('Error al obtener los datos:', error);
-      });
-  }, []);
-
   return (
     <DrawerResponsive>
       <HomePageComponent/>
