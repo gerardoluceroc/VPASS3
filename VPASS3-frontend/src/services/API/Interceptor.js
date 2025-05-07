f53589d0d4a078d385c9ed78c70e5b4097bd9d86
gerardoluceroc <gerardo.lucero.c@usach.cl>
2025-05-07 17:21:00 -0400

    probando cosas en interceptor

--- VPASS3-frontend/src/services/API/Interceptor.js
+++ VPASS3-frontend/src/services/API/Interceptor.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 // Define una función que configura un interceptor para las solicitudes salientes de Axios
 export function InterceptorRequest() {
+    
     // Obtiene el estado del usuario desde Redux utilizando useSelector
     const data = useSelector(state => state.user);
 
@@ -27,12 +28,19 @@ export function InterceptorRequest() {
     );
 }
 
-
-
 export function InterceptorResponse(){
+    console.log("Ingresando al interceptor de respuesta");
     axios.interceptors.response.use(
       response => {
-          return response;
+            if(response?.status === 401){
+                console.log("Interceptando respuesta, esta es: ", response);
+                alert("Su sesión ha expirado, por favor inicie sesión nuevamente.");
+                // Aquí puedes redirigir al usuario a la página de inicio de sesión o realizar otra acción
+                window.location.href = "/login";
+            }
+            else{
+                return response;
+            }
       },
       error => {
           // Caso 1: El cliente no tiene conexión a internet o el servidor no responde
@@ -40,25 +48,7 @@ export function InterceptorResponse(){
               console.error("Error de red o sin conexión a internet: ", error.message);
               return Promise.reject({statusCode: 0, data: null, message: "Error de red o sin conexión a internet"});
           }
-  
-          // Caso 2: La respuesta del servidor tiene un código de error (4xx, 5xx)
-          // const status = error.response.status;
-          // if (status >= 400 && status < 500) {
-          //     console.error(`Error en la solicitud: Código ${status} - `, error.response.data);
-          //     return Promise.reject({
-          //         message: `Error del cliente: Código ${status} - ${error.response.data.message || "Error en la petición."}`
-          //     });
-          // }
-  
-          // // Caso 3: Error del servidor (5xx)
-          // if (status >= 500) {
-          //     console.error(`Error en el servidor: Código ${status} - `, error.response.data);
-          //     return Promise.reject({
-          //         message: `Error del servidor: Código ${status} - ${error.response.data.message || "Problema en el servidor."}`
-          //     });
-          // }
-          // Rechazar el error si no cae en los casos anteriores
           return Promise.reject(error);
       }
     );
-  }
+}
