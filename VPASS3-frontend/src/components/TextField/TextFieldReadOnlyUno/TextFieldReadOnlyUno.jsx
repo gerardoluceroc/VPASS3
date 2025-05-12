424a7cd445ed75869325af4ac7560cb6dfce5316
gerardoluceroc <gerardo.lucero.c@usach.cl>
2025-05-12 16:44:27 -0400

    Creacion de componente textfield de solo lectura

---
+++ VPASS3-frontend/src/components/TextField/TextFieldReadOnlyUno/TextFieldReadOnlyUno.jsx
@@ -0,0 +1,31 @@
+import { TextField } from "@mui/material";
+
+const TextFieldReadOnlyUno = ({ label, value, borderBottomColorAfter = "#175676", borderBottomColorBefore = "#175676", labelColor = "#175676", multiline = false, maxRowsMultiline = 2, width = "100%" }) => {
+    return (
+      <TextField
+        id="standard-read-only-input"
+        label={label}
+        value={value || ""}  // Asegúrate de que `value` nunca sea `undefined`
+        variant="standard"
+        multiline = {multiline}
+        maxRows={maxRowsMultiline}
+        InputProps={{
+          readOnly: true,
+        }}
+        sx={{
+          "& .MuiInput-underline:before": {
+            borderBottomColor: borderBottomColorBefore,  // Color de la línea cuando no está seleccionada
+          },
+          "& .MuiInput-underline:after": {
+            borderBottomColor: borderBottomColorAfter,  // Color de la línea cuando está seleccionada
+          },
+          // Estilo para el label
+          "& .MuiInputLabel-root": {
+            color: labelColor,  // Color del label por defecto
+          },
+          width: width,
+        }}
+      />
+    );
+  }
+  export default TextFieldReadOnlyUno;
