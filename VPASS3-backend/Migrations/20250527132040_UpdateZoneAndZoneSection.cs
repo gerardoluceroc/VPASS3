22e4f0643d4fa32483f7505413a0f7e6ec90e951
gerardoluceroc <gerardo.lucero.c@usach.cl>
2025-05-27 16:57:20 -0400

    Actualizacion migraciones

---
+++ VPASS3-backend/Migrations/20250527132040_UpdateZoneAndZoneSection.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VPASS3_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateZoneAndZoneSection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ZoneSections",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Zones",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Blacklists",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ZoneSections");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Zones");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Blacklists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
