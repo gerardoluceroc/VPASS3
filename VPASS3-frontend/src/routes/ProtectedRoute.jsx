54a4ff4b213b5e1539ab63ab94eb31f025c2b43e
gerardoluceroc <gerardo.lucero.c@usach.cl>
2025-06-05 16:40:01 -0400

    eliminacion de codigo comentado

--- VPASS3-frontend/src/routes/ProtectedRoute.jsx
+++ VPASS3-frontend/src/routes/ProtectedRoute.jsx
@@ -9,8 +9,6 @@ import { RUTA_LOGIN } from "../utils/rutasCliente";
 
 const ProtectedRoute = ({ children }) => {
 
-  // InterceptorResponse();
-  // InterceptorRequest();
   const {logoutSession} = useLogin();
 
   const [showExpiredSessionDialog, setShowExpiredSessionDialog] = useState(false);
